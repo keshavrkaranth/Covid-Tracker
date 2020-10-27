b319c8ccc48d7b1bf88f11322865dc4fa78388e9
keshavrkaranth <keshavarkarantha@gmail.com>
2020-10-27 17:31:22 +0530

    addded virtual env

--- Covid/settings.py
+++ Covid/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
+SECRET_KEY = '*8y=g)%zc&8y)er@)lier#06r6)+tebbp!c-+mdm88p(76m#*1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
